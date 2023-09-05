2b1ff379ce33e3c8a7cd72ef6c280e8eb8d5c9a1
alevargasa90 <alevargasa90@live.seminolestate.edu>
2023-09-04 20:39:22 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,20 @@
 package fibonacci;
 
+/**
+ * This class contains methods for calculating Fibonacci numbers.
+ *
+ */
+
 public class Fibonacci {
+	
+	/**
+	 * Calculates the nth term of the Fibonacci sequence.
+	 * 
+	 * @param n The position in the Fibonacci sequence (positive integer or 0).
+	 * @return The nth term of the Fibonacci sequence.
+	 * @throws IllegalArgumentException if n is not positive integer or 0. 
+	 */
+	
 	public static int calculateFibonacci(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n must be a positive integer.");
@@ -13,6 +27,12 @@ public class Fibonacci {
         }
     }
 
+	/**
+	 * Main method that calls the calculateFibonacci method and outputs the result.
+	 * 
+	 * @param args The command-line arguments
+	 */
+	
     public static void main(String[] args) {
         int n = 10;
         int result = calculateFibonacci(n);
